6223f725b6b3313d6768d2393dcd8b613cc34893
Gareth Smith <gas001@gmail.com>
2017-10-16 23:06:56 +0100

    Tweaking styling

--- wp-content/themes/newrytennis/page.php
+++ wp-content/themes/newrytennis/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col-sm-8">
+    <div class="col-sm-8 plain-main">
 
         <?php 
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
