4e904e8ed06e05fba5ff5e3dcd4e5413eb974114
Gareth Smith <gas001@gmail.com>
2017-10-16 21:51:24 +0100

    improving styling of single post page

--- wp-content/themes/newrytennis/blog_content.php
+++ wp-content/themes/newrytennis/blog_content.php
@@ -1,14 +1,14 @@
 <div class="blog-post">
     <p><a class="blog-post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
     <?php echo styled_category_list(); ?>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author_posts_link(); ?></a></p>
 
     <?php if ( has_post_thumbnail() ) {?>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <?php	the_post_thumbnail('thumbnail'); ?>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <?php the_excerpt(); ?>
             </div>
         </div>
@@ -16,7 +16,7 @@
 	   <?php the_excerpt(); ?>
 	<?php } ?>
     
-    <a href="<?php comments_link(); ?>" class="blog-post-comment-link">
+    <a href="<?php comments_link(); ?>" title="View comments" class="blog-post-comment-link">
         <?php
         printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( get_comments_number() ) ); ?>
     </a>
--- wp-content/themes/newrytennis/blog_content_single.php
+++ wp-content/themes/newrytennis/blog_content_single.php
@@ -1,13 +1,8 @@
 <div class="blog-post">
-    <h2 class="blog-post-title">
-        <?php the_title(); ?>
-    </h2>
-    <p class="blog-post-meta">
-        <?php the_date(); ?> by
-        <a href="#">
-            <?php the_author(); ?>
-        </a>
-    </p>
+    <p class="blog-post-title single-blog-post-title"><?php the_title(); ?></p>
+    <?php echo styled_category_list(); ?>
+    <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author_posts_link(); ?></a></p>
+
     <?php the_content(); ?>
 </div>
 <!-- /.blog-post -->
