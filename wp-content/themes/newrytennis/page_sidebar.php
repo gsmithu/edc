7aa2acbad169f843df84f4a18f935e5e26442110
Gareth Smith <gas001@gmail.com>
2018-03-10 20:45:19 +0000

    More styling tweaks

--- wp-content/themes/newrytennis/page_sidebar.php
+++ wp-content/themes/newrytennis/page_sidebar.php
@@ -1,10 +1,15 @@
 <div class="col-sm-3 col-sm-offset-1 sidebar">
     <div class="sidebar-module sidebar-module-inset">
-        <h4>Playing Hours</h4>
-        <ol class="list-unstyled">
-            <li><b><i>Mon-Fri</i></b> - <?php echo get_option('playingHoursMonFri'); ?></li>
-            <li><b><i>Sat</i></b> - <?php echo get_option('playingHoursSat'); ?></li>
-            <li><b><i>Sun</i></b> - <?php echo get_option('playingHoursSun'); ?></li>
+        <h4>Members' Hours</h4>
+        <ol class="list-unstyled playing-hours-col">
+            <li><b>Mon-Fri</b></li>
+            <li><b>Sat</b></li>
+            <li><b>Sun</b></li>
+        </ol>
+        <ol class="list-unstyled playing-hours-col">
+            <li><?php echo get_option('playingHoursMonFri'); ?></li>
+            <li><?php echo get_option('playingHoursSat'); ?></li>
+            <li><?php echo get_option('playingHoursSun'); ?></li>
         </ol>
     </div>
     <div class="sidebar-module">
