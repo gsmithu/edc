68ab3c3f69825eb2c577df76e22cafd3d23df2e2
Gareth Smith <gas001@gmail.com>
2018-03-10 18:09:45 +0000

    Styling sidebar

--- wp-content/themes/newrytennis/sidebar.php
+++ wp-content/themes/newrytennis/sidebar.php
@@ -1,13 +1,5 @@
 <div class="col-sm-3 col-sm-offset-1 sidebar">
-<!--
     <div class="sidebar-module sidebar-module-inset">
-        <h4>About</h4>
-        <p>
-            <?php echo get_bloginfo( 'description' ); ?>
-        </p>
-    </div>
--->
-    <div class="sidebar-module-inset">
         <h4>Archives</h4>
         <ol class="list-unstyled">
             <?php wp_get_archives( 'type=monthly' ); ?>
